d440d1ccd81ebaacc4296e74c8cee10764ec928b
0jeannette <88587476+0jeannette@users.noreply.github.com>
2021-09-04 21:18:32 -0400

    Add files via upload

--- PyPoll.py
+++ PyPoll.py
@@ -1,3 +1,4 @@
+
 #Add dependencies
 import csv 
 import os
@@ -5,24 +6,20 @@ import os
 file_to_load = os.path.join("Resources", "election_results.csv")
 #Assign variable to save file to path 
 file_to_save = os.path.join("analysis", "election_analysis.txt")
-
 #initializing vote counter
 total_votes = 0
-
+#candidate options
 candidate_options = []
 candidate_votes = {}
-
+#track winning candidate
 winning_candidate = ""
 winning_count = 0
 winning_percentage = 0
-
 #Open election results and read file
 with open(file_to_load) as election_data:
     file_reader = csv.reader(election_data)
-
     #Read and print header row
     headers = next(file_reader) 
-
     #Print each row in the CSV file
     for row in file_reader:
         total_votes += 1
@@ -32,6 +29,7 @@ with open(file_to_load) as election_data:
             candidate_votes[candidate_name] = 0
         candidate_votes[candidate_name] += 1
 
+#Save results to text file
 with open(file_to_save, "w") as txt_file:
     election_results = (
         f"\nElection Results\n"
@@ -47,22 +45,18 @@ with open(file_to_save, "w") as txt_file:
         candidate_results = (f"{candidate_name}: {vote_percentage:.1f}% ({votes:,})\n")
         print(candidate_results)
         txt_file.write(candidate_results)
-
-
         if (votes > winning_count) and (vote_percentage > winning_percentage):
             winning_count = votes
             winning_candidate = candidate_name
             winning_percentage = vote_percentage
-
     winning_candidate_summary = (
-    f"-------------------------\n"
-    f"Winner: {winning_candidate}\n"
-    f"Winning Vote Count: {winning_count:,}\n"
-    f"Winning Percentage: {winning_percentage:.1f}%\n"
-    f"-------------------------\n")
-
-
+        f"-------------------------\n"
+        f"Winner: {winning_candidate}\n"
+        f"Winning Vote Count: {winning_count:,}\n"
+        f"Winning Percentage: {winning_percentage:.1f}%\n"
+        f"-------------------------\n")
     print(winning_candidate_summary)
-
+    txt_file.write(winning_candidate_summary)
+    
     
 election_data.close()
